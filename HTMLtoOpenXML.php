a1a27a625856abd6ca993d8574b80414a626c662
richard korebrits <richard@korebrits.com>
2018-04-20 11:03:27 +0200

    Update parsing
    Fixed $input in `processBreaks`
    
    Fixed lists in `processStyle`
    
    Improved phpdoc in `fromHTML`

--- HTMLtoOpenXML.php
+++ HTMLtoOpenXML.php
@@ -1,40 +1,46 @@
 <?php
 
-class HTMLtoOpenXML {
-	
-	
+class HTMLtoOpenXML
+{
+
+
 	/**
 	 * The only one instance of this class.
 	 * @var HTMLtoOpenXML
 	 */
 	private static $_instance;
-	 
+
 	/**
 	 * Private constructor of singleton.
 	 */
-	private function __construct() {
+	private function __construct()
+	{
 		require_once "Scripts/HTMLCleaner.php";
 		require_once "Scripts/ProcessProperties.php";
 	}
-	
+
 	/**
 	 * Return the singleton instance. Creates one if no one.
 	 */
-	public static function getInstance() {
-		if (is_null(self::$_instance)){
+	public static function getInstance()
+	{
+		if ( is_null(self::$_instance) )
+		{
 			self::$_instance = new HTMLtoOpenXML();
 		}
+
 		return self::$_instance;
 	}
-	
+
 	/**
 	 * Converts HTML to RTF.
 	 *
-	 * @param input
-	 * 		the HTML formated input string
-	 * @return The converted string.
+	 * @param string $htmlCode the HTML formated input string
+	 *
+	 * @return string The converted string.
 	 */
-	public function fromHTML($htmlCode) {
+	public function fromHTML($htmlCode)
+	{
 		$start = 0;
 		$properties = array();
 		$openxml = HTMLCleaner::getInstance()->cleanUpHTML($htmlCode);
@@ -44,44 +50,52 @@ class HTMLtoOpenXML {
 		$openxml = ProcessProperties::getInstance()->processPropertiesStyle($openxml, $start, $properties);
 		$openxml = $this->processSpaces($openxml);
 		$openxml = $this->processStyle($openxml);
-		
+
 		return $openxml;
 	}
-	
-	private function getOpenXML($text) {
+
+	private function getOpenXML($text)
+	{
 		$text = "<w:p><w:r><w:t>$text</w:t></w:r></w:p>";
+
 		return $text;
 	}
-	
-	private function processListStyle($input) {
+
+	private function processListStyle($input)
+	{
 		$output = preg_replace("/(<ul>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $input);
 		$output = preg_replace("/(<\/ul>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $output);
 		$output = preg_replace("/(<ol>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $output);
 		$output = preg_replace("/(<\/ol>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $output);
 		$output = preg_replace("/(<li>)/mi", "</w:t></w:r><w:p startliste><w:r><w:t>", $output);
 		$output = preg_replace("/(<\/li>)/mi", "", $output);
+
 		return $output;
 	}
-	
-	private function processBreaks($input) {
+
+	private function processBreaks($input)
+	{
 		$output = preg_replace("/(<\/p>)/mi", "</w:t></w:r></w:p><w:p><w:r><w:t>", $input);
-		$output = preg_replace("/(<br>)/mi", "</w:t></w:r></w:p><w:p><w:r><w:t>", $input);
+		$output = preg_replace("/(<br\s?\/?>)/mi", "</w:t></w:r></w:p><w:p><w:r><w:t>", $output);
+
 		return $output;
 	}
-	
-	private function processSpaces($input) {
+
+	private function processSpaces($input)
+	{
 		$output = preg_replace("/(&nbsp;)/mi", " ", $input);
 		$output = preg_replace("/(<w:t>)/mi", "<w:t xml:space='preserve'>", $output);
+
 		return $output;
 	}
-	
-	private function processStyle($input) {
+
+	private function processStyle($input)
+	{
 		$output = preg_replace("/(<w:p>)/mi", "<w:p><w:pPr><w:pStyle w:val='OurStyle2'/></w:pPr>", $input);
-		$output = preg_replace("/(<w:p startliste>)/mi", "</w:p><w:p><w:pPr><w:pStyle w:val='BulletStyle'/></w:pPr>", $output);
+		$output = preg_replace("/(<w:p startliste>)/mi", "</w:p><w:p><w:pPr><w:pStyle w:val='BulletStyle'/><w:numPr><w:ilvl w:val='0'/><w:numId w:val='3'/></w:numPr></w:pPr>", $output);
+
 		return $output;
 	}
-	
-	
+
+
 }
-	
-?>
