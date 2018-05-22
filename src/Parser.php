7072c574181f9affca318d554fd7412eb75fd48e
richard korebrits <richard@korebrits.com>
2018-05-22 16:35:38 +0200

    List handling and clean whitespace
    Little bit experimental, but this handles multple lists better. Instead
    of keep counting lists, it restarts the count for each list.
    
    Also implemented better whitespace cleaning so lines start at the
    beginning

--- src/Parser.php
+++ src/Parser.php
@@ -7,6 +7,7 @@ class Parser
 
     private $_cleaner;
     private $_processProperties;
+    private $_listIndex = 1;
 
     /**
      * Private constructor of singleton.
@@ -28,12 +29,14 @@ class Parser
         $start = 0;
 
         $openxml = $this->_cleaner->cleanUpHTML($htmlCode);
-        if($wrapContent) {
+        if ($wrapContent) {
             $openxml = $this->getOpenXML($openxml);
         }
         $openxml = $this->processBreaks($openxml);
         $openxml = $this->processListStyle($openxml);
-        $openxml = $this->_processProperties->processPropertiesStyle($openxml, $start);
+        $openxml = $this->_processProperties->processPropertiesStyle(
+                $openxml, $start
+        );
         $openxml = $this->processSpaces($openxml);
         $openxml = $this->processStyle($openxml);
 
@@ -47,24 +50,43 @@ class Parser
     }
 
     private function processListStyle($input) {
-        $output = preg_replace(
-                "/(<ul>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $input
+        $output = $input;
+
+        $output = preg_replace("/\n/", ' ', $output);
+        $output = preg_replace_callback(
+                '/<(ul|ol).*?>(.*?)<\/(?:ul|ol)>/im', [$this, 'processList'],
+                $output
         );
-        $output = preg_replace(
-                "/(<\/ul>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $output
-        );
-        $output = preg_replace(
-                "/(<ol>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $output
-        );
-        $output = preg_replace(
-                "/(<\/ol>)/mi", '</w:t></w:r></w:p><w:p><w:r><w:t>', $output
-        );
-        $output = preg_replace(
-                "/(<li>)/mi", "</w:t></w:r><w:p startliste><w:r><w:t>", $output
-        );
-        $output = preg_replace("/(<\/li>)/mi", "", $output);
 
         return $output;
+    }
+
+    public function processList($html) {
+
+        $output = '';
+
+        $output .= preg_replace_callback(
+                '/<li.*?>(.*?)<\/li>/im', [$this, 'processListItem'], $html[2]
+        );
+
+        $output .= '</w:t></w:r></w:p><w:p><w:r><w:t>';
+
+        // Add a blank line after the list, otherwise it's attached
+        $output .= '</w:t></w:r></w:p><w:p><w:r><w:t>';
+
+        $this->_listIndex += 1;
+
+        return $output;
+    }
+
+    public function processListItem($html) {
+
+        $html = sprintf(
+                "</w:t></w:r></w:p><w:p><w:pPr><w:pStyle w:val='ListParagraph'/><w:numPr><w:ilvl w:val='0'/><w:numId w:val='%d'/></w:numPr></w:pPr><w:r><w:t xml:space='preserve'>%s",
+                $this->_listIndex, trim($html[1])
+        );
+
+        return $html;
     }
 
     private function processBreaks($input) {
@@ -84,6 +106,29 @@ class Parser
                 "/(<w:t>)/mi", "<w:t xml:space='preserve'>", $output
         );
 
+        $re
+                = '%# Collapse whitespace everywhere but in blacklisted elements.
+        (?>             # Match all whitespans other than single space.
+          [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+        | \s{2,}        # or two or more consecutive-any-whitespace.
+        ) # Note: The remaining regex consumes no text at all...
+        (?=             # Ensure we are not in a blacklist tag.
+          [^<]*+        # Either zero or more non-"<" {normal*}
+          (?:           # Begin {(special normal*)*} construct
+            <           # or a < starting a non-blacklist tag.
+            (?!/?(?:textarea|pre|script)\b)
+            [^<]*+      # more non-"<" {normal*}
+          )*+           # Finish "unrolling-the-loop"
+          (?:           # Begin alternation group.
+            <           # Either a blacklist start tag.
+            (?>textarea|pre|script)\b
+          | \z          # or end of file.
+          )             # End alternation group.
+        )  # If we made it here, we are not in a blacklist tag.
+        %Six';
+
+        $output = preg_replace($re, "", $output);
+
         return $output;
     }
 
@@ -92,14 +137,8 @@ class Parser
                 "/(<w:p>)/mi", "<w:p><w:pPr><w:pStyle w:val='OurStyle2'/></w:pPr>",
                 $input
         );
-        $output = preg_replace(
-                "/(<w:p startliste>)/mi",
-                "</w:p><w:p><w:pPr><w:pStyle w:val='BulletStyle'/><w:numPr><w:ilvl w:val='0'/><w:numId w:val='3'/></w:numPr></w:pPr>",
-                $output
-        );
 
         return $output;
     }
-
 
 }
