f4e7fd204be543f056c6da05c2e6508a1d891a15
richard korebrits <richard@korebrits.com>
2018-06-21 15:10:33 +0200

    Add "support" for nested lists
    Nothing fancy, just makes it not break. Not possible to make actual
    nested lists without access to `numbering.xml`

--- src/Parser.php
+++ src/Parser.php
@@ -9,6 +9,8 @@ class Parser
     private $_processProperties;
     private $_listIndex = 1;
 
+    const BREAK = '</w:t></w:r><w:r><w:br/><w:t>';
+
     /**
      * Private constructor of singleton.
      */
@@ -33,7 +35,7 @@ class Parser
             $openxml = $this->getOpenXML($openxml);
         }
         $openxml = $this->processBreaks($openxml);
-        $openxml = $this->processListStyle($openxml);
+        $openxml = $this->_processListStyle($openxml);
         $openxml = $this->_processProperties->processPropertiesStyle(
                 $openxml, $start
         );
@@ -43,24 +45,26 @@ class Parser
         $openxml = $this->processSpaces($openxml);
         $openxml = $this->processStyle($openxml);
 
-
+        //prd($openxml);
         return $openxml;
     }
 
-    private function _removeEndSpaces($openxml){
+    private function _removeEndSpaces($openxml) {
         $regex = '/<w:t><\/w:t><\/w:r><\/w:p><w:p><w:r><w:t>$/';
-        if(preg_match($regex, $openxml)) {
+        if (preg_match($regex, $openxml)) {
             $openxml = preg_replace($regex, '<w:t>', $openxml);
+
             return $this->_removeEndSpaces($openxml);
         }
 
         return $openxml;
     }
 
-    private function _removeStartSpaces($openxml){
+    private function _removeStartSpaces($openxml) {
         $regex = '/^<w:p><w:r><w:t><\/w:t><\/w:r><\/w:p>/';
-        if(preg_match($regex, $openxml)) {
+        if (preg_match($regex, $openxml)) {
             $openxml = preg_replace($regex, '', $openxml);
+
             return $this->_removeStartSpaces($openxml);
         }
 
@@ -73,10 +77,57 @@ class Parser
         return $text;
     }
 
-    private function processListStyle($input) {
-        $output = $input;
 
-        $output = preg_replace("/\n/", ' ', $output);
+    /**
+     * We can't process nested lists unfortunately, as we would need to write
+     * this to numbering.xml. Stead we replace by dash or numbers
+     *
+     * @param $input
+     *
+     * @return string
+     */
+    private function _preProcessNestedLists($html) {
+        $output = preg_replace_callback(
+                '/<li>(.*?)<(?:ul|ol).*?>(.*?)<\/(?:ul|ol).*?><\/li>/im',
+                [$this, 'preProcessNestedList'], $html
+        );
+        $output = $output;
+
+        return $output;
+    }
+
+    public function preProcessNestedList($html) {
+
+        $text = preg_replace_callback(
+                '/<li.*?>(.*?)<\/li>/im', [$this, 'preProcessNestedListItems'],
+                $html[2]
+        );
+
+        $output = '<li>';
+        if ($html[1]) {
+            $output .= $html[1] . self::BREAK;
+        }
+        $output .= $text;
+        $escaped_break = str_replace('/', '\/', self::BREAK);
+        $output = preg_replace('/' . $escaped_break . '$/', '', $output);
+        $output .= '</li>';
+
+        return $output;
+    }
+
+    public function preProcessNestedListItems($html) {
+
+        $output = ' - ' . $html[1] . self::BREAK;
+
+        return $output;
+    }
+
+    private function _processListStyle($input) {
+        $output = preg_replace("/\n/", ' ', $input);
+
+
+        $output = $this->_preProcessNestedLists($output);
+
         $output = preg_replace_callback(
                 '/<(ul|ol).*?>(.*?)<\/(?:ul|ol)>/im', [$this, 'processList'],
                 $output
@@ -126,11 +177,12 @@ class Parser
 
     /**
      * @param $html
+     *
      * @author © Alex Moore
      *
      * @return null|string|string[]
      */
-    public function minifyHtml($html){
+    public function minifyHtml($html) {
         $re
                 = '%# Collapse whitespace everywhere but in blacklisted elements.
         (?>             # Match all whitespans other than single space.
